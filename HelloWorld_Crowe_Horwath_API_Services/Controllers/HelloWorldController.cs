649951ea4f6bcb9ab97f7df543cb1aac680d4afb
Pushpak Panchal <pushpak.net@gmail.com>
2020-08-26 16:37:25 -0500

    adding api services project

---
+++ HelloWorld_Crowe_Horwath_API_Services/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorld_Crowe_Horwath_API_Services.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
